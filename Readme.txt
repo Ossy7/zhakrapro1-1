e8cf2a9269956d7f6cfcff31742905fcdabf1fd8
Daniel <dan.ossy.do@gmail.com>
2018-05-29 21:58:26 +0200

    Add files via upload

--- Readme.txt
+++ Readme.txt
@@ -1,35 +1,49 @@
-ZhakraPro
-version: 1.1.0
+ZhakraPro1-3
+version: 1.3.0
 
-Zhakrapro version 1.1.0 is a digital forensics GUI app written in Python 2.7.14+ and PyQt4.
-It is a file integrity validator, system and device internet monitor. 
-ZhakraPro 1.1.0 was built and tested on both 32/64bits linux machines.
-The updated version now includes internet metrics, system storage evaluation and the ability to track geolocation of established connections.
+Zhakrapro is a digital forensics GUI app written in Python 2.7.14+ and PyQt4.
+It is a file integrity validator, system and device internet monitor.
+ZhakraPro was built and tested on both 64 and 32 bits linux  machines.
 
 Internet Monitor:
 1. View device established connections
 2. View running processes within device
 3. View running processes and users of those processes
 4. View network statistics.
-5. View tcp metrics
-6. View storage space
-7. IP/Website geolocation tracker.
+5. Stop process
+6. View tcp metrics
+7. View geolocation of connected IP address.
+
+File Modification History
+Enter path to the directory where you intend to view its file modification, example /home/username
+Then click view history
 
 File Validation:
 1. Enter File path
 2. Select a Hash Function
 
-Using zhakrapro1-1 in its simplest form:
-unzip zhakrapro1-1-dist-linux-x86_64.zip as the case maybe and scroll down to zhakrapro1-1 and double click.
-Viola!
-
 Licence:
-ZhakraPro version 1.1.0 is licenced under the GNU General Public Licence.
-ZhakraPro 1.1.0 is distrisbuted in the hope that it will be useful BUT WITHOUT ANY WARRANTY.
+ZhakraPro is licenced under the GNU General Public Licence.
+ZhakraPro is distrisbuted in the hope that it will be useful BUT WITHOUT ANY WARRANTY.
 See the GNU General Public Licence for more details.
+
+General instructions:
+1 For file integrity check, specify full file path. Example: /home/username/myfile.zip
+2 Select a hash function to use in integrity check.
+
+Using Zhakrapro in its simplest form:
+1. Unzip zhakrapro1-3-dist-linux-x86_64.zip as the case maybe, scroll to zhakrapro application and double click.
+Viola !
+
+Integrity check:
+sha256 zhakrapro1-3-dist-linux-x86_64.zip: e93d411dd93048010e4461a4206d73c4c0549d6a0e999ea45b63472bbefb73dd 
+
+sha256 zhakrapro1-3-dist-linux_x86.zip:
+6928a34771accf402b42f4c24349bbafbb9ba3b5e34bbd190665f9b44b5521d8 
+
 
 Developer:
 Daniel Osinachi N.
 dan.ossy.do@gmail.com
 Copyright (C) 2018 Daniel Osinachi N.
-24-03-2018
+29-05-2018
---
+++ zhakrapro1-3.py
@@ -0,0 +1,343 @@
+import hashlib
+import GeoIP
+import sys
+import subprocess
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+__version__ = "1.3.0"
+
+BLOCKSIZE = 65536
+
+class ZhakraPro(QTabWidget):
+    def __init__(self, parent=None):
+        super(ZhakraPro, self).__init__(parent)
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tab3 = QWidget()
+        self.tab4 = QWidget()
+
+        self.addTab(self.tab1, "File Integrity")
+        self.addTab(self.tab2, "View Internet Connections")
+        self.addTab(self.tab3, "File Modification History")
+        self.addTab(self.tab4, "Help")
+
+        self.tab1UI()
+        self.tab2UI()
+        self.tab3UI()
+        self.tab4UI()
+
+        
+    def tab1UI(self):
+        self.lineedit_fname = QLineEdit("Enter File name or file path")
+
+        self.combobox_options = QComboBox()
+        self.combobox_options.addItems(["Select Hash Function", "SHA1", "SHA256", "SHA384", "SHA512", "MD5"])
+
+        self.btn_fhash = QPushButton("Hash File")
+        self.browser = QTextBrowser()
+        self.browser.setAlignment(Qt.AlignCenter)
+        
+        vbox1 = QVBoxLayout()
+        vbox1.addWidget(self.lineedit_fname)
+        vbox1.addWidget(self.combobox_options)
+        
+        vbox1.addWidget(self.browser)
+        self.tab1.setLayout(vbox1)
+        self.setGeometry(10, 10, 1100, 500)
+        
+        self.connect(self.combobox_options,
+                     SIGNAL("currentIndexChanged(int)"), self.updateUi)
+        self.setWindowTitle("ZhakraPro File Integrity Validator and Internet Monitor")
+
+    def tab2UI(self):
+        vbox2 = QVBoxLayout()
+        hbox = QHBoxLayout()
+        
+        self.btn_acon = QPushButton("View Established Connections")
+        self.btn_acon.clicked.connect(self.viewCon)
+        
+        self.btn_rprocess = QPushButton("View Running Processes")
+        self.btn_rprocess.clicked.connect(self.viewProc)
+
+        self.btn_stat = QPushButton("View Statistics")
+        self.btn_stat.clicked.connect(self.nt_usage)
+
+        self.btn_proc = QPushButton("View Process Users")
+        self.btn_proc.clicked.connect(self.pusers)
+        
+        self.browser1 = QTextBrowser()
+        self.browser1.setAlignment(Qt.AlignCenter)
+
+        self.btn_metrics = QPushButton("View Metrics")
+        self.btn_metrics.clicked.connect(self.metrics)
+
+        self.btn_pkiller = QPushButton("Stop Process")
+        self.btn_pkiller.clicked.connect(self.pkiller)
+
+        self.btn_tracker = QPushButton("IP Geolocator")
+        self.btn_tracker.clicked.connect(self.locator)
+
+        hbox.addWidget(self.btn_acon)
+        hbox.addWidget(self.btn_rprocess)
+        hbox.addWidget(self.btn_stat)
+        hbox.addWidget(self.btn_proc)
+
+        hbox.addWidget(self.btn_metrics)
+        hbox.addWidget(self.btn_pkiller)
+        hbox.addWidget(self.btn_tracker)
+        vbox2.addLayout(hbox)
+
+        vbox2.addWidget(self.browser1)
+        self.tab2.setLayout(vbox2)
+
+    def tab3UI(self):
+        self.le_dir = QLineEdit("Directory or Folder Path.")
+        self.btn_vmodif = QPushButton("View File History")
+        self.btn_vmodif.clicked.connect(self.fhistory)
+        self.browser2 = QTextBrowser()
+
+        vbox3 = QVBoxLayout()
+        vbox3.addWidget(self.le_dir)
+        vbox3.addWidget(self.btn_vmodif)
+        vbox3.addWidget(self.browser2)
+        self.tab3.setLayout(vbox3)
+        
+        
+
+            
+    def tab4UI(self):
+        self.browser3 = QTextBrowser()
+        self.browser3.setText("This application is written in Python 2.7.14 and PyQt4."+"\n"\
+                             +"Version 1.3.0" + "\n"\
+                             +"author: Daniel Osinachi N." +"\n"\
+                              +"dan.ossy.do@gmail.com"+"\n"\
+                              +"Copyright (C) 2018 Daniel Osinachi N.")
+            
+        vbox4 = QVBoxLayout()
+        vbox4.addWidget(self.browser3)
+        self.tab4.setLayout(vbox4)
+
+    
+
+    def updateUi(self):
+        ind = int(self.combobox_options.currentIndex())
+
+        if ind == 1:
+            self.Zhak_sha1()
+        elif ind == 2:
+            self.Zhak_sha256()
+        elif ind == 3:
+            self.Zhak_sha384()
+        elif ind == 4:
+            self.Zhak_sha512()
+        elif ind == 5:
+            self.Zhak_md5()
+            
+            
+
+    def Zhak_sha1(self):
+        fname = str(self.lineedit_fname.text())
+        
+        hasher = hashlib.sha1()
+        try:
+            with open(fname, 'rb') as afile_tohash:
+                buf = afile_tohash.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile_tohash.read(BLOCKSIZE)
+                    chksum1 = hasher.hexdigest()
+                    self.browser.setText("The SHA1 Checksum for " + " %s " % fname + " : " + " %s "  % chksum1)
+        except Exception, IOError:
+            self.browser.setText("No such file in your current directory, specify file path.")
+
+    def Zhak_sha256(self):
+        
+        fname = str(self.lineedit_fname.text())
+        
+        hasher = hashlib.sha256()
+        try:
+            with open(fname, 'rb') as afile_tohash:
+                buf = afile_tohash.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile_tohash.read(BLOCKSIZE)
+                    chksum2 = hasher.hexdigest()
+                    self.browser.setText("The SHA256 Checksum for " + " %s " % fname + " : " + " %s "  % chksum2)
+        except Exception, IOError:
+            self.browser.setText("No such file in your current directory, specify file path.")
+
+    def Zhak_sha384(self):
+        fname = str(self.lineedit_fname.text())
+        
+        hasher = hashlib.sha384()
+        try:
+            with open(fname, 'rb') as afile_tohash:
+                buf = afile_tohash.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile_tohash.read(BLOCKSIZE)
+                    chksum3 = hasher.hexdigest()
+                    self.browser.setText("The SHA384 Checksum for " + " %s " % fname + " : " + " %s "  % chksum3)
+        except Exception, IOError:
+            self.browser.setText("No such file in your current directory, specify file path.")
+
+    def Zhak_sha512(self):
+        fname = str(self.lineedit_fname.text())
+        
+        hasher = hashlib.sha512()
+        try:
+            with open(fname, 'rb') as afile_tohash:
+                buf = afile_tohash.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile_tohash.read(BLOCKSIZE)
+                    chksum5 = hasher.hexdigest()
+                    self.browser.setText("The SHA512 Checksum for " + " %s " % fname + " : " + " %s "  % chksum5)
+        except Exception, IOError:
+            self.browser.setText("No such file in your current directory, specify file path.")
+
+    def Zhak_md5(self):
+        fname = str(self.lineedit_fname.text())
+        
+        hasher = hashlib.md5()
+        try:
+            with open(fname, 'rb') as afile_tohash:
+                buf = afile_tohash.read(BLOCKSIZE)
+                while len(buf) > 0:
+                    hasher.update(buf)
+                    buf = afile_tohash.read(BLOCKSIZE)
+                    chksumM5 = hasher.hexdigest()
+                    self.browser.setText("The MD5 for " + " %s " % fname + " : " + " %s "  % chksumM5)
+        except Exception, IOError:
+            self.browser.setText("No such file in your current directory, specify file path.")
+
+
+
+    def viewCon(self):
+        activeCon = subprocess.check_output(['netstat', '-atulpn'])
+        self.browser1.setText(activeCon)
+
+    def viewProc(self):
+        activePro = subprocess.check_output(['netstat', '-alpn'])
+        self.browser1.setText(activePro)
+
+    def nt_usage(self):
+        nt_stat = subprocess.check_output(['netstat', '-s'])
+        self.browser1.setText(nt_stat)
+        
+
+    def pusers(self):
+        runningp = subprocess.check_output(['ps', 'aux'])
+        self.browser1.setText(runningp)
+
+
+    def metrics(self):
+        m_tcp = subprocess.check_output(['ip', 'tcp_metrics'])
+        self.browser1.setText(m_tcp)
+
+    def pkiller(self):
+        text, ok = QInputDialog.getText(self, "Process Killer", "Enter the PID:")
+        if ok and text:
+            user_text = str(text)
+            killer = subprocess.check_output(['kill', user_text])
+            self.browser1.setText(killer)
+
+    def locator(self):
+        items = ("Website", "IPv4", "IPv6")
+        item, ok = QInputDialog.getItem(self, "IP-Domain Geolocator",
+                                        "List of available options", items, 0, False)
+        if ok and item:
+            if item == "Website":
+                self.locate_website()
+            elif item == "IPv4":
+                self.locate_ipv4()
+            elif item == "IPv6":
+                self.locate_ipv6()
+    
+    def locate_website(self):
+        text, ok = QInputDialog.getText(self, "Website Geolocator", "Enter the website:")
+        if ok and text:
+            user_text = unicode(text)
+            gi = GeoIP.open("/usr/share/GeoIP/GeoIPCity.dat", GeoIP.GEOIP_STANDARD)
+            gir = gi.record_by_name(user_text) or gi.record_by_addr(user_text)
+
+            if gir is not None:
+                data = gir
+                country_code = data['country_code']
+                country_code3 = data['country_code3']
+                country_name = data['country_name']
+                city = data['city']
+                region = data['region']
+                region_name = data['region_name']
+                postal_code = data['postal_code']
+                lat = data['latitude']
+                lon = data['longitude']
+                area_code = data['area_code']
+                time_zone = data['time_zone']
+                metro_code = data['metro_code']
+                self.browser1.setText("Country:" "%s" "\n"
+                                      "City:" "%s" "\n"
+                                      "Region:" "%s" "\n"
+                                      "Latitude:" "%s" "\n"
+                                      "Longitude:" "%s" "\n"
+                                      "Time Zone:" "%s" %(country_name, city, region_name, lat, lon, time_zone))
+                
+    def locate_ipv4(self):
+        text, ok = QInputDialog.getText(self, "IP Geolocator", "Enter the IP address:")
+        if ok and text:
+            user_text = unicode(text)
+            gi = GeoIP.open("/usr/share/GeoIP/GeoIPCity.dat", GeoIP.GEOIP_STANDARD)
+            gir = gi.record_by_name(user_text) or gi.record_by_addr(user_text)
+
+            if gir is not None:
+                data = gir
+                country_code = data['country_code']
+                country_code3 = data['country_code3']
+                country_name = data['country_name']
+                city = data['city']
+                region = data['region']
+                region_name = data['region_name']
+                postal_code = data['postal_code']
+                lat = data['latitude']
+                lon = data['longitude']
+                area_code = data['area_code']
+                time_zone = data['time_zone']
+                metro_code = data['metro_code']
+                self.browser1.setText("Country:" "%s" "\n"
+                                      "City:" "%s" "\n"
+                                      "Region:" "%s" "\n"
+                                      "Latitude:" "%s" "\n"
+                                      "Longitude:" "%s" "\n"
+                                      "Time Zone:" "%s" %(country_name, city, region_name, lat, lon, time_zone))
+                
+                
+                                                     
+                                                     
+    def locate_ipv6(self):
+        text, ok = QInputDialog.getText(self, "IPv6 Geolocator", "Enter the IPv6 address:")
+        if ok and text:
+            user_text = unicode(text)
+            gi = GeoIP.open("/usr/share/GeoIP/GeoIPv6.dat", GeoIP.GEOIP_STANDARD)
+            gir = gi.country_name_by_name_v6(user_text) or gi.country_name_by_addr_v6(user_text)
+            if gir is not None:
+                self.browser1.setText(str(gir))
+
+    def fhistory(self):
+        if self.le_dir.text():
+            dpath = str(self.le_dir.text())
+            try:
+                fh = subprocess.check_output(['ls', '-actl', dpath])
+                self.browser2.setText(fh)
+            except Exception, e:
+                self.browser2.setText("[*]Please ensure it is a directory or folder path and You have access permission.")
+        else:
+            self.browser2.setText("[*]...Please specify a path to a folder or directory.")
+            
+app = QApplication(sys.argv)
+form = ZhakraPro()
+form.show()
+app.exec_()
+                
+        
+                     
+        
